c4dd63a67362d29b797ea9d9dff45ebe45e43084
whonkie <hallwes57@gmail.com>
2019-01-16 17:07:48 -0800

    changed return base.GetLetter Grade to return 'F'

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -29,8 +29,6 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else
                 return 'F';
-
-            return base.GetLetterGrade(averageGrade);
         }
     }
 }
